6e12dacb943fb89e3ff7c280b7a86e9895baa6ff
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2024-09-29 00:04:06 -0400

    Ensuring bigram model can be successfully run

--- server.py
+++ server.py
@@ -18,6 +18,10 @@ def home():
 @app.route('/sentiment')
 def sentiment():
     return render_template('sentiment.html')
+
+@app.route('/bigram')
+def bigram():
+    return render_template('bigram.html')
     
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -37,7 +41,51 @@ def predict():
     
     return render_template('sentiment_result.html', result=str(compound), sentence=features)
     # return str(compound)
+
+
+def sentence(start_word, max_length):
+    current_word = start_word
+    sentence = [current_word]
+
+    #Loops a number of times decided by max_length
+    for _ in range(max_length - 1):
+        if current_word not in model_bigram_news.index:
+            break  # Stop if the current word is not in the table
+
+        # Get the probabilities of the next words
+        next_word_probs = model_bigram_news.loc[current_word]
+
+        # Filter out words with zero probability
+        next_word_probs = next_word_probs[next_word_probs > 0]
+
+        if next_word_probs.empty:
+            break  # Stop if there are no valid next words
+
+        # Chooses the next word randomly based on probability distribution
+        next_word = np.random.choice(next_word_probs.index, p=next_word_probs.values)
+
+        # Adds new word to end of sentence
+        sentence.append(next_word)
+        current_word = next_word  # Move to the next word
+
+        # Set '.' as a sentence terminator
+        if next_word == '.':
+          # Breaks if a '.' is the next probable token
+          break
+
+    return render_template('bigram_result.html', sentence=' '.join(sentence))
     
+@app.route('/generate_sentence', methods=['POST'])
+def generate_sentence():
+    # data = request.get_json(force=True)
+
+    # try:
+    #     features = data['features']
+    # except KeyError:
+    #     return jsonify(error="The 'features' key is missing from the request payload.")
+    data = request.form['start_word']
+
+    return sentence(data, 20)
 
 if __name__ == '__main__':
     app.run(debug=True)
